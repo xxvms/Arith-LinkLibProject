ce190f478da07bdec6fbce2234cbb1307e35b024
Tom Breza <tom@pcservicegroup.co.uk>
2017-05-26 10:49:52 +0100

    Add project files.

---
+++ Arith-LinkLibProject/Main.cpp
@@ -0,0 +1,13 @@
+#include "HomeworkExercise2-Library/arith.h"
+#include <iostream>
+
+int main()
+{
+	Arith arith;
+
+	std::cout << arith.Square(3) << std::endl;
+
+
+	system("pause");
+	return 0;
+}
